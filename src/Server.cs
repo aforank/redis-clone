e942b2b82986dbdb69d63c05939fe21d8ef74fc4
Ankit Sharma <anks@microsoft.com>
2024-05-14 17:53:26 +0530

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -8,17 +8,25 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-var socket = await server.AcceptSocketAsync(); // wait for client1
 
 while(true)
 {
-    using var stream = new NetworkStream(socket);
-    var buffer = new byte[1024];
-    var bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
-    var request = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-    Console.WriteLine(request);
-    var response = Encoding.UTF8.GetBytes("+PONG\r\n");
-    await stream.WriteAsync(response, 0, response.Length);
+    var socket = await server.AcceptSocketAsync(); // wait for client1
+
+    var _ = Task.Run(async () => await HandleClient(socket));
 }
 
+static async Task HandleClient(Socket socket)
+{
+    while (true)
+    {
+        using var stream = new NetworkStream(socket);
+        var buffer = new byte[1024];
+        var bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+        var request = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        Console.WriteLine(request);
+        var response = Encoding.UTF8.GetBytes("+PONG\r\n");
+        await stream.WriteAsync(response, 0, response.Length);
+    }
+}
 
